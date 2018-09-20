b000b06d700f7bb0a727ff268d0651edc2126f1b
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-09-20 16:54:19 +0700

    Initial Commit

---
+++ TestImportCPPClass/Assets/TestImportCPPClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestImportCPPClass : MonoBehaviour
+{
+    [DllImport("TestExportCPPClass")]
+    public static extern IntPtr Student_Create();
+
+    [DllImport("TestExportCPPClass")]
+    public static extern void Student_SetScore(IntPtr value, int score);
+
+    [DllImport("TestExportCPPClass")]
+    public static extern int Student_GetGrade(IntPtr value);
+
+    [DllImport("TestExportCPPClass")]
+    public static extern void Student_Delete(IntPtr value);
+
+    void Start()
+    {
+        int[] scores = {20, 40, 50, 56, 60, 75, 85, 100};
+        IntPtr[] students = new IntPtr[scores.Length];
+        for (int i = 0; i < students.Length; i++)
+        {
+            int score = scores[i];
+            students[i] = Student_Create();
+            Student_SetScore(students[i], score);
+        }
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            int grade = Student_GetGrade(students[i]);
+            Debug.Log("Score: " + scores[i] + " Grade: " + grade);
+            Student_Delete(students[i]);
+        }
+    }
+}
